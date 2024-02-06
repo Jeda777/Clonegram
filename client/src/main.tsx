4eb33b3defc9712869e3e65bbfaa95c7e367198c
Jeda777 <jakub.jeda@wp.pl>
2024-02-06 19:37:16 +0100

    Created form layout for auth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 import App from './pages/App.tsx'
+import SignIn from './pages/Sign-in.tsx'
 
 import './index.css'
-import SignIn from './pages/Sign-in.tsx'
+
+const themeObject = {
+  resetCSS: true,
+  colors: {
+    text: {
+      secondary: '#c7c7ca',
+    },
+  },
+}
+
+const theme = extendTheme(themeObject)
 
 const router = createBrowserRouter([
   {
@@ -17,6 +29,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>,
 )
--- client/src/pages/Sign-in.tsx
+++ client/src/pages/Sign-in.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { Center, Heading, FormControl, FormLabel, Input, Flex, Button } from '@chakra-ui/react'
+import { useState } from 'react'
 
 const SignIn = () => {
-  return <div>Sign-in</div>
+  const [register, setRegister] = useState(false)
+
+  return (
+    <Center w='100vw' h='100vh'>
+      <Flex flexDirection='column' gap={4} p={4} border='1px' borderColor='black' rounded={16}>
+        <Heading alignSelf='center'>Clonegram</Heading>
+        <FormControl display='flex' flexDirection='column'>
+          <FormLabel>Email</FormLabel>
+          <Input mb={2} type='text' />
+          {register && (
+            <>
+              <FormLabel>Username</FormLabel>
+              <Input mb={2} type='text' />
+            </>
+          )}
+          <FormLabel>Password</FormLabel>
+          <Input mb={2} type='text' />
+          <Button type='submit' colorScheme='messenger'>
+            {register ? 'Sign Up' : 'Sign In'}
+          </Button>
+        </FormControl>
+        <Button
+          type='submit'
+          variant='link'
+          textColor='text.secondary'
+          onClick={() => {
+            setRegister((p) => !p)
+          }}
+        >
+          {register ? 'Already Signed up?' : `Don't have an account?`}
+        </Button>
+      </Flex>
+    </Center>
+  )
 }
 
 export default SignIn
