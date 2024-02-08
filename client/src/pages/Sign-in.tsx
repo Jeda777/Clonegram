fb538b8eca5de64d879e2f901de0730a079df6bb
Jeda777 <jakub.jeda@wp.pl>
2024-02-08 19:54:03 +0100

    Added checkbox for persist login option

--- client/src/pages/Sign-in.tsx
+++ client/src/pages/Sign-in.tsx
@@ -1,11 +1,21 @@
-import { Center, Heading, Flex, Button, Box, useColorModeValue } from '@chakra-ui/react'
-import { useState } from 'react'
+import { Center, Heading, Flex, Button, Box, useColorModeValue, Checkbox } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import RegisterForm from '../components/authForms/RegisterForm'
 import LoginForm from '../components/authForms/LoginForm'
 import ThemeButton from '../components/ThemeButton'
+import useAuth from '../hooks/useAuth'
 
 const SignIn = () => {
   const [SignUp, setSignUp] = useState(false)
+  const { persist, setPersist } = useAuth()
+
+  const handlePersist = () => {
+    setPersist((prev) => !prev)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('persist', String(persist))
+  }, [persist])
 
   return (
     <Center w='100vw' h='100vh'>
@@ -15,6 +25,9 @@ const SignIn = () => {
       <Flex flexDirection='column' gap={4} p={4} border='1px' borderColor={useColorModeValue('black', 'white')} rounded={16}>
         <Heading alignSelf='center'>Clonegram</Heading>
         {SignUp ? <RegisterForm /> : <LoginForm />}
+        <Checkbox colorScheme='messenger' defaultChecked={persist} onChange={handlePersist}>
+          Trust this device
+        </Checkbox>
         <Button
           type='submit'
           variant='link'
