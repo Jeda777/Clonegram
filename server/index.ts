f5459251231059dd7e24b9e4d4639b1d519ea59c
Jeda777 <jakub.jeda@wp.pl>
2024-02-08 14:59:40 +0100

    Updated allowed headers

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true, allowedHeaders: 'Content-Type' }))
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true, allowedHeaders: ['Content-Type', 'Authorization'] }))
 
 app.use(router)
 
