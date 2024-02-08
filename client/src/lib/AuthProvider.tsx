17ff6a3dd9641039f79f27678917eb06aba2afec
Jeda777 <jakub.jeda@wp.pl>
2024-02-08 12:30:17 +0100

    Update AuthProvider.tsx
    Added types

--- client/src/lib/AuthProvider.tsx
+++ client/src/lib/AuthProvider.tsx
@@ -1,9 +1,20 @@
 import React, { createContext, useState } from 'react'
+import { authObject } from '../../types'
 
-const authContext = createContext({})
+type authContextProps = {
+  auth: authObject | {}
+  setAuth: ({}: authObject | {}) => void
+}
+
+const initialValue = {
+  auth: {},
+  setAuth: () => {},
+}
+
+const authContext = createContext<authContextProps>(initialValue)
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [auth, setAuth] = useState({})
+  const [auth, setAuth] = useState(initialValue.auth)
 
   return <authContext.Provider value={{ auth, setAuth }}>{children}</authContext.Provider>
 }
