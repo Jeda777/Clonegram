82c8354e2ef8ed465c001f6cacf26ee9d4100f7e
Jeda777 <jakub.jeda@wp.pl>
2024-02-08 17:43:18 +0100

    useRefreshToken changed to use custom axios instance

--- client/src/hooks/useRefreshToken.ts
+++ client/src/hooks/useRefreshToken.ts
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from '../api/axios'
 import useAuth from './useAuth'
 
 const useRefreshToken = () => {
   const { setAuth, auth } = useAuth()
 
   const refresh = async () => {
-    const result = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/refresh`, { withCredentials: true })
+    const result = await axios.get('/refresh', { withCredentials: true })
     const currentAuth = auth
     setAuth({ ...currentAuth, accessToken: result.data.accessToken })
     return result.data.accessToken
