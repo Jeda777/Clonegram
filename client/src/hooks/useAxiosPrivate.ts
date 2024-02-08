3e20ec7956288506fe6753776d92eb727cf89ceb
Jeda777 <jakub.jeda@wp.pl>
2024-02-08 14:48:27 +0100

    Created useAxiosPrivateHook
    Created useAxiosPrivateHook for updating access token when sending request to protected routes

---
+++ client/src/hooks/useAxiosPrivate.ts
@@ -0,0 +1,53 @@
+import { axiosPrivate } from '../api/axios'
+import { useEffect } from 'react'
+import useRefreshToken from './useRefreshToken'
+import useAuth from './useAuth'
+import { AxiosError } from 'axios'
+
+const useAxiosPrivate = () => {
+  const refresh = useRefreshToken()
+  const { auth } = useAuth()
+
+  useEffect(() => {
+    const requestInterceptor = axiosPrivate.interceptors.request.use(
+      (config) => {
+        if (!config.headers['Authorization']) {
+          config.headers['Authorization'] = `Bearer ${auth.accessToken}`
+        }
+        return config
+      },
+      (error) => Promise.reject(error),
+    )
+
+    const responseInterceptor = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error: AxiosError) => {
+        const prevRequest = error.config
+        //@ts-ignore
+        if (error.response?.status === 403 && !prevRequest?.sent) {
+          //@ts-ignore
+          prevRequest.sent = true
+
+          const newAccessToken = await refresh()
+
+          //@ts-ignore
+          prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
+
+          //@ts-ignore
+          return axiosPrivate(prevRequest)
+        }
+
+        return Promise.reject(error)
+      },
+    )
+
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestInterceptor)
+      axiosPrivate.interceptors.response.eject(responseInterceptor)
+    }
+  }, [auth, refresh])
+
+  return axiosPrivate
+}
+
+export default useAxiosPrivate
