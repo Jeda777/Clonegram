5be4b10551efcdba2201e92102f5be7c2f2fa6ed
Jeda777 <jakub.jeda@wp.pl>
2024-02-10 20:41:45 +0100

    Added theme toggle to mobile nav

--- client/src/components/navigation/mobile/MobileNavTop.tsx
+++ client/src/components/navigation/mobile/MobileNavTop.tsx
@@ -1,12 +1,20 @@
-import { Flex, IconButton } from '@chakra-ui/react'
-import { Send } from 'lucide-react'
+import { Flex, IconButton, useColorMode } from '@chakra-ui/react'
+import { Moon, Send, Sun } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 
 const MobileNavTop = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
   const navigate = useNavigate()
 
   return (
-    <Flex width='100%' position='fixed' top={0} justifyContent='end' py={2} px={7}>
+    <Flex width='100%' position='fixed' top={0} justifyContent='end' gap={2} py={2} px={7}>
+      <IconButton
+        aria-label='Theme switch'
+        icon={colorMode === 'dark' ? <Sun /> : <Moon />}
+        variant='ghost'
+        isRound
+        onClick={() => toggleColorMode()}
+      />
       <IconButton aria-label='Conversations' icon={<Send />} variant='ghost' isRound onClick={() => navigate('/conversations')} />
     </Flex>
   )
