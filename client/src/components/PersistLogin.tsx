676bd736d12ebbf7dd85168f9800f2efd4ad1eed
Jeda777 <jakub.jeda@wp.pl>
2024-02-10 19:40:23 +0100

    Loading component with persist login
    Created loading component, render loading component while isLoading is true on persist login component

--- client/src/components/PersistLogin.tsx
+++ client/src/components/PersistLogin.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import useRefreshToken from '../hooks/useRefreshToken'
 import useAuth from '../hooks/useAuth'
+import Loading from './Loading'
 
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true)
@@ -27,7 +28,7 @@ const PersistLogin = () => {
     }
   }, [])
 
-  return <>{!persist ? <Outlet /> : isLoading ? <p>Loading</p> : <Outlet />}</>
+  return <>{!persist ? <Outlet /> : isLoading ? <Loading /> : <Outlet />}</>
 }
 
 export default PersistLogin
