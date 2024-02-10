fef7b4a7a6935721b553a44af38e2a626caec714
Jeda777 <jakub.jeda@wp.pl>
2024-02-10 20:49:00 +0100

    Added  backgrounds to mobile nav and higher zindex

--- client/src/components/navigation/mobile/MobileNavBottom.tsx
+++ client/src/components/navigation/mobile/MobileNavBottom.tsx
@@ -1,4 +1,4 @@
-import { Flex, IconButton } from '@chakra-ui/react'
+import { Flex, IconButton, useColorModeValue } from '@chakra-ui/react'
 import { CircleUserRound, Home, ImagePlus, Search } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 import useAuth from '../../../hooks/useAuth'
@@ -8,7 +8,16 @@ const MobileNavBottom = () => {
   const navigate = useNavigate()
 
   return (
-    <Flex width='100%' position='fixed' bottom={0} justifyContent='space-between' py={2} px={7}>
+    <Flex
+      width='100%'
+      position='fixed'
+      bottom={0}
+      justifyContent='space-between'
+      py={2}
+      px={7}
+      background={useColorModeValue('gray.50', 'gray.900')}
+      zIndex={10}
+    >
       <IconButton aria-label='Home' icon={<Home />} variant='ghost' isRound onClick={() => navigate('/')} />
       <IconButton aria-label='Search' icon={<Search />} variant='ghost' isRound />
       <IconButton aria-label='Add post' icon={<ImagePlus />} variant='ghost' isRound />
--- client/src/components/navigation/mobile/MobileNavTop.tsx
+++ client/src/components/navigation/mobile/MobileNavTop.tsx
@@ -1,4 +1,4 @@
-import { Flex, IconButton, useColorMode } from '@chakra-ui/react'
+import { Flex, IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { Moon, Send, Sun } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 
@@ -7,7 +7,17 @@ const MobileNavTop = () => {
   const navigate = useNavigate()
 
   return (
-    <Flex width='100%' position='fixed' top={0} justifyContent='end' gap={2} py={2} px={7}>
+    <Flex
+      width='100%'
+      position='fixed'
+      top={0}
+      justifyContent='end'
+      gap={2}
+      py={2}
+      px={7}
+      background={useColorModeValue('gray.50', 'gray.900')}
+      zIndex={10}
+    >
       <IconButton
         aria-label='Theme switch'
         icon={colorMode === 'dark' ? <Sun /> : <Moon />}
