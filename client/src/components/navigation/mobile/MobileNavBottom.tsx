8540d2c8ffc3e35236ba8f49589dee28700caa0f
Jeda777 <jakub.jeda@wp.pl>
2024-02-10 20:30:36 +0100

    Created basic mobile navigation
    Created components for top and bottom mobile navigation

---
+++ client/src/components/navigation/mobile/MobileNavBottom.tsx
@@ -0,0 +1,26 @@
+import { Flex, IconButton } from '@chakra-ui/react'
+import { CircleUserRound, Home, ImagePlus, Search } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+import useAuth from '../../../hooks/useAuth'
+
+const MobileNavBottom = () => {
+  const { auth } = useAuth()
+  const navigate = useNavigate()
+
+  return (
+    <Flex width='100%' position='fixed' bottom={0} justifyContent='space-between' py={2} px={7}>
+      <IconButton aria-label='Home' icon={<Home />} variant='ghost' isRound onClick={() => navigate('/')} />
+      <IconButton aria-label='Home' icon={<Search />} variant='ghost' isRound />
+      <IconButton aria-label='Home' icon={<ImagePlus />} variant='ghost' isRound />
+      <IconButton
+        aria-label='Home'
+        icon={<CircleUserRound />}
+        variant='ghost'
+        isRound
+        onClick={() => navigate(`/user/${auth.username}`)}
+      />
+    </Flex>
+  )
+}
+
+export default MobileNavBottom
---
+++ client/src/components/navigation/mobile/MobileNavTop.tsx
@@ -0,0 +1,15 @@
+import { Flex, IconButton } from '@chakra-ui/react'
+import { Send } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
+const MobileNavTop = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Flex width='100%' position='fixed' top={0} justifyContent='end' py={2} px={7}>
+      <IconButton aria-label='Home' icon={<Send />} variant='ghost' isRound onClick={() => navigate('/conversations')} />
+    </Flex>
+  )
+}
+
+export default MobileNavTop
