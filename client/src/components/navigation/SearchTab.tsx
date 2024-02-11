e09fc0d1dd81f09de1b14f955e357c2b0cf554cf
Jeda777 <jakub.jeda@wp.pl>
2024-02-11 12:19:51 +0100

    Adjusted search tab position and padding

--- client/src/components/navigation/SearchTab.tsx
+++ client/src/components/navigation/SearchTab.tsx
@@ -1,22 +1,27 @@
-import { Flex, Input, useBreakpoint, useColorModeValue } from '@chakra-ui/react'
+import { Flex, Input, useColorModeValue } from '@chakra-ui/react'
 import { useAppSelector } from '../../hooks/useReduxHooks'
 
 const SearchTab = () => {
   const tabsState = useAppSelector((state) => state.tabs)
   const isOpen = tabsState.isOpen && tabsState.type === 'search'
-  const breakpoint = useBreakpoint()
+
+  const leftClose = [0, null, '-100%']
+  const leftOpen = [0, null, '208px', '224px', '272px', '336px']
+  const topClose = ['-100%', null, 0]
+  const topOpen = 0
+
   return (
     <Flex
       flexDirection='column'
       background={useColorModeValue('white', 'gray.800')}
       zIndex={5}
       width={['100%', null, 'auto']}
-      pt={breakpoint === 'base' || breakpoint === 'sm' ? 16 : 14}
+      pt={[16, null, 14]}
       pb={14}
       px={7}
       position='fixed'
-      left={breakpoint === 'base' || breakpoint === 'sm' ? '0' : isOpen ? '0' : '-100%'}
-      top={(breakpoint === 'base' && !isOpen) || (breakpoint === 'sm' && !isOpen) ? '-100%' : '0'}
+      left={isOpen ? leftOpen : leftClose}
+      top={isOpen ? topOpen : topClose}
       height='100vh'
     >
       <Input />
