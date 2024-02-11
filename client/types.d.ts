648ba777bd477868f54e4d87958f020b44fd2895
Jeda777 <jakub.jeda@wp.pl>
2024-02-11 15:16:46 +0100

    Added types for data from /user/:username endpoint

--- client/types.d.ts
+++ client/types.d.ts
@@ -5,3 +5,32 @@ export type authObject = {
   imageUrl: string
   username: string
 }
+
+export type api_user_username_data_user = {
+  description: string | null
+  followersCount: number
+  followingCount: number
+  id: string
+  imageUrl: string
+  postsCount: number
+  private: boolean
+  username: string
+}
+
+export type api_user_username_data_post = {
+  id: string
+  userId: string
+  imageUrl: string
+  createdAt: Date
+  commentsCount: number
+  likesCount: number
+}
+
+type api_user_username_data = {
+  user: api_user_username_data_user
+  posts: api_user_username_data_post[]
+  isFollowing: boolean
+  isRequested: boolean
+  isOwnUser: boolean
+  isAllowed: boolean
+}
